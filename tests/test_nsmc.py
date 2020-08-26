e574a639b7f950f5cfebf580acae233b86aa027c
lovit <soy.lovit@gmail.com>
2020-08-26 23:37:11 +0900

    Pytest: NSMC usgae

---
+++ tests/test_nsmc.py
@@ -0,0 +1,18 @@
+from Korpora import NSMCData
+from Korpora import Korpora
+
+
+def test_usage()
+    root_dir = 'path/to/Korpora/'
+    nsmc = NSMCData(root_dir)
+    assert len(nsmc.train.texts) == 150000
+    assert len(nsmc.train.labels) == 150000
+    assert len(nsmc.test.texts) == 50000
+    assert len(nsmc.test.labels) == 50000
+    assert len(nsmc.get_all_labels()) == 200000
+
+    nsmc = Korpora.load('nsmc', root_dir)
+    assert len(nsmc.train.texts) == 150000
+    assert len(nsmc.train.labels) == 150000
+    assert len(nsmc.test.texts) == 50000
+    assert len(nsmc.test.labels) == 50000
