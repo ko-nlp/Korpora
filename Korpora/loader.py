73f5f726affb34c70e0a774ea46343dae6c27b94
lovit <soy.lovit@gmail.com>
2020-08-27 01:10:01 +0900

    Available to use `force_download` in Korpora.load

--- Korpora/loader.py
+++ Korpora/loader.py
@@ -3,8 +3,15 @@ from .utils import default_korpora_path
 
 
 class Korpora:
+    """
+    Examples::
+        >>> from Korpora import Korpora
+        >>> nsmc = Korpora.load('nsmc')
+        >>> len(nsmc.train.texts)   # 150000
+        >>> len(nsmc.train.labels)  # 50000
+    """
     @classmethod
-    def load(cls, corpus_names, root_dir=None):
+    def load(cls, corpus_names, root_dir=None, force_download=False):
         return_single = isinstance(corpus_names, str)
         if return_single:
             corpus_names = [corpus_names]
@@ -12,7 +19,7 @@ class Korpora:
         if root_dir is None:
             root_dir = default_korpora_path
 
-        corpora = [KORPORA[corpus_name](root_dir) for corpus_name in corpus_names]
+        corpora = [KORPORA[corpus_name](root_dir, force_download) for corpus_name in corpus_names]
         if return_single:
             return corpora[0]
         return corpora
