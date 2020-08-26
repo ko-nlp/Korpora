9e8c77f972da1a137848a23cb1b296093b1f5d12
lovit <soy.lovit@gmail.com>
2020-08-26 23:29:56 +0900

    WIP: fetch

--- Korpora/fetch.py
+++ Korpora/fetch.py
@@ -1,15 +1,32 @@
-from .utils import default_korpora_path
-
-def fetch(corpus_name, root_dir=None):
-    if root_dir is None:
-        root_dir = default_korpora_path
-    DATA_FETCH(corpus_name)(root_dir)
+from .utils import download, check_dir
 
 
-def fetch_gitclone(root_dir):
-    raise NotImplementedError
+def fetch(corpus_name, root_dir):
+    if corpus_name not in DATA_LOCATIONS:
+        raise ValueError(f'support only {set(DATA_LOCATIONS.keys())}')
+    locations = DATA_LOCATIONS[corpus_name]
+    for location in locations:
+        if location['method'] == 'download':
+            do_download(location)
+
+def do_download(information, root_dir):
+    url = information['url']
+    destination = os.path.join(root_dir, information['destination'])
+#    check_dir(destination)
+#    download(url, destination)
 
 
-DATA_FETCH = {
-    'nsmc': fetch_gitclone,
+DATA_LOCATIONS = {
+    'nsmc': [
+        {
+            'url': 'https://raw.githubusercontent.com/e9t/nsmc/master/ratings_train.txt',
+            'destination': 'nsmc/rating_train.txt',
+            'method': 'download'
+        },
+        {
+            'url': 'https://raw.githubusercontent.com/e9t/nsmc/master/ratings_test.txt',
+            'destination': 'nsmc/rating_test.txt',
+            'method': 'download'
+        },
+    ],
 }
