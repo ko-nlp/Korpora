131db1a3e1aa1601592cedaabc2ff00500a75a6b
lovit <soy.lovit@gmail.com>
2020-08-29 17:25:20 +0900

    Korean petition Korpora class (#5)

--- Korpora/__init__.py
+++ Korpora/__init__.py
@@ -1,5 +1,6 @@
 from .about import __author__   # noqa: F401
 from .about import __version__  # noqa: F401
 
+from .korpora_korean_petitions import KoreanPetitions
 from .korpora_nsmc import NSMC
 from .loader import Korpora
