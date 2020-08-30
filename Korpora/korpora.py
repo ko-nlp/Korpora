f9681d7d01917feade4407bbcdb152be1fd809a1
lovit <soy.lovit@gmail.com>
2020-08-31 01:21:14 +0900

    Show description and attributes Korpus, KorpusData, NSMC, NSMCData (fix #14)

--- Korpora/korpora.py
+++ Korpora/korpora.py
@@ -6,13 +6,34 @@ from .utils import default_korpora_path
 
 @dataclass
 class KorpusData:
+    description: str
     texts: List[str]
 
     def __len__(self):
         return len(self.texts)
 
+    def __str__(self):
+        classname = self.__class__.__name__
+        spec = ""
+        for name, var in self.__dict__.items():
+            if name not in {'description', 'self'}:
+                spec += f'  {classname}.{name} (list[{var[0].__class__.__name__}]) : size={len(var)}\n'
+        s = f"""{classname}\n{self.description}\n\nAttributes:\n{spec}\n"""
+        return s
+
 
 class Korpus:
+    description: str
+    license: str
+
+    def __str__(self):
+        classname = self.__class__.__name__
+        s = f"{classname}\n{self.description}\n\nAttributes\n"
+        for name, var in self.__dict__.items():
+            if name not in {'description', 'license', 'self'}:
+                s += f' {classname}.{name} : size={len(var)}\n'
+        return s
+
     def cleaning(self, raw_documents: List[str], **kargs):
         """`raw_data` to `sentences`"""
         raise NotImplementedError('Implement this function')
