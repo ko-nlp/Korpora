0c6971f14a577209f99a78c11408daaf78bbae0e
gichang.lee <ratsgo@naver.com>
2020-08-31 00:50:51 +0900

    PR test, error case (#16)

--- tests/test_nsmc.py
+++ tests/test_nsmc.py
@@ -1,11 +1,11 @@
-from Korpora import NSMCData
+from Korpora.korpora_nsmc import NSMCData
 from Korpora import Korpora
 
 
-def test_usage()
+def test_usage():
     root_dir = 'path/to/Korpora/'
     nsmc = NSMCData(root_dir)
-    assert len(nsmc.train.texts) == 150000
+    assert len(nsmc.train.texts) == 150001
     assert len(nsmc.train.labels) == 150000
     assert len(nsmc.test.texts) == 50000
     assert len(nsmc.test.labels) == 50000
