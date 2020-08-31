75db1b690c22f2e87b94c9521fecd4265960b509
lovit <soy.lovit@gmail.com>
2020-09-01 02:01:04 +0900

    Show description and attributes (fix#14)

--- Korpora/korpora.py
+++ Korpora/korpora.py
@@ -4,6 +4,7 @@ from typing import List
 
 @dataclass
 class KorpusData:
+    description: str
     texts: List[str]
 
     def __len__(self):
@@ -12,8 +13,28 @@ class KorpusData:
     def __getitem__(self):
         raise NotImplementedError('Implement __getitem__')
 
+    def __str__(self):
+        classname = self.__class__.__name__
+        spec = ""
+        for name, var in self.__dict__.items():
+            if name not in {'description', 'self'}:
+                spec += f'  {classname}.{name} (list[{var[0].__class__.__name__}]) : size={len(var)}\n'
+        s = f"""{classname}\n{self.description}\n\nAttributes:\n{spec}\n"""
+        return s
+
 
 class Korpus:
+    description: str
+    license: str
+
+    def __str__(self):
+        classname = self.__class__.__name__
+        s = f"{classname}\n{self.description}\n\nAttributes\n"
+        for name, var in self.__dict__.items():
+            if name not in {'description', 'license', 'self'}:
+                s += f' {classname}.{name} : size={len(var)}\n'
+        return s
+
     def cleaning(self, raw_documents: List[str], **kargs):
         """`raw_data` to `sentences`"""
         raise NotImplementedError('Implement this function')
