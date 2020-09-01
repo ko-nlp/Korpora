e4d45d7b126da45a0808d99468ac3a59e337ad4b
gichang.lee <ratsgo@naver.com>
2020-09-01 15:34:21 +0900

    check nsmc license&description (#16)

--- tests/test_nsmc.py
+++ tests/test_nsmc.py
@@ -8,3 +8,6 @@ def test_usage():
     assert len(nsmc.train.labels) == 150000
     assert len(nsmc.test.texts) == 50000
     assert len(nsmc.test.labels) == 50000
+    assert 'Naver sentiment movie corpus v1.0' in nsmc.description
+    assert 'CC0 1.0 Universal' in nsmc.license
+
