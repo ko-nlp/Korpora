5bf1733ca841708db9f8b39dc883d01a56f1fb25
gichang.lee <ratsgo@naver.com>
2020-09-01 14:24:34 +0900

    update nsmc test case (#16)

--- tests/test_nsmc.py
+++ tests/test_nsmc.py
@@ -1,16 +1,8 @@
-from Korpora.korpora_nsmc import NSMCData
 from Korpora import Korpora
 
 
 def test_usage():
-    root_dir = 'path/to/Korpora/'
-    nsmc = NSMCData(root_dir)
-    assert len(nsmc.train.texts) == 150001
-    assert len(nsmc.train.labels) == 150000
-    assert len(nsmc.test.texts) == 50000
-    assert len(nsmc.test.labels) == 50000
-    assert len(nsmc.get_all_labels()) == 200000
-
+    root_dir = './data/'
     nsmc = Korpora.load('nsmc', root_dir)
     assert len(nsmc.train.texts) == 150000
     assert len(nsmc.train.labels) == 150000
