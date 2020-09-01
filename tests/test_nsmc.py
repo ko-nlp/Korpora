eef392f602c713ccf645a2bbc7dbb53f24c2e95e
gichang.lee <ratsgo@naver.com>
2020-09-01 18:12:32 +0900

    valid NSMC (#28)

--- tests/test_nsmc.py
+++ tests/test_nsmc.py
@@ -1,19 +1,11 @@
-import Korpora
-from Korpora import NSMCData
 from Korpora import Korpora
 
 
 def test_usage():
-    root_dir = 'path/to/Korpora/'
-    nsmc = NSMCData(root_dir)
+    nsmc = Korpora.load('nsmc')
     assert len(nsmc.train.texts) == 150000
     assert len(nsmc.train.labels) == 150000
     assert len(nsmc.test.texts) == 50000
     assert len(nsmc.test.labels) == 50000
-    assert len(nsmc.get_all_labels()) == 200000
-
-    nsmc = Korpora.load('nsmc', root_dir)
-    assert len(nsmc.train.texts) == 150000
-    assert len(nsmc.train.labels) == 150000
-    assert len(nsmc.test.texts) == 50000
-    assert len(nsmc.test.labels) == 50000
+    assert 'Naver sentiment movie corpus v1.0' in nsmc.description
+    assert 'CC0 1.0 Universal' in nsmc.license
