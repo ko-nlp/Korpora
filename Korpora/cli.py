a90063ec19f21032d170064e79e6d18893513c1c
lovit <soy.lovit@gmail.com>
2020-10-06 17:10:27 +0900

    CLI fetch (#108)

---
+++ Korpora/cli.py
@@ -0,0 +1,56 @@
+import argparse
+import os
+
+from .about import __version__
+from .loader import Korpora, FETCH
+
+
+def fetch(args):
+    corpus_names = args.corpus
+    if corpus_names == 'all':
+        Korpora.fetch(corpus_names, args.root, args.force_download)
+        return
+
+    if isinstance(corpus_names, str):
+        corpus_names = [corpus_names]
+    for name in corpus_names:
+        if name not in FETCH:
+            print(f'Korpora does not provide `{name}` corpus')
+            continue
+        Korpora.fetch(name, args.root, args.force_download)
+
+
+def show_arguments(args):
+    print('## Arguments of Moducorpus sanitizer ##')
+    for name, var in sorted(vars(args).items()):
+        if callable(var):
+            print(f'  - {name} : {var.__name__}')
+        else:
+            print(f'  - {name} : {var}')
+
+
+def show_version(args):
+    print(f'moducorpus_sanitizer=={__version__}')
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Korpora Command Line Interface')
+    parser.set_defaults(func=show_version)
+    subparsers = parser.add_subparsers(help='Korpora')
+
+    # fetch
+    parser_fetch = subparsers.add_parser('fetch', help='Tokenize `input` and save the result to `output`')
+    parser_fetch.add_argument('--corpus', type=str, default='all', nargs='+', help='corpus type')
+    parser_fetch.add_argument('--root', type=str, default=None, help='path/to/Korpora/')
+    parser_fetch.add_argument('--force_download', dest='force_download', action='store_true')
+    parser_fetch.set_defaults(func=fetch)
+
+    # Do task
+    args = parser.parse_args()
+    show_arguments(args)
+    task_function = args.func
+    task_function(args)
+
+
+if __name__ == '__main__':
+    main()
