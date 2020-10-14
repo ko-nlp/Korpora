7852188df851bcd2a2dc90e526c4e04c6b54e6ed
lovit <soy.lovit@gmail.com>
2020-10-15 02:32:40 +0900

    Import `description`, `license`, `fetch_modu` from `korpus_modu_news.py` (#118)

--- Korpora/korpus_modu_morpheme.py
+++ Korpora/korpus_modu_morpheme.py
@@ -7,22 +7,7 @@ from tqdm import tqdm
 from typing import List, Tuple
 from Korpora.korpora import Korpus, KorpusData
 
-
-description = """    모두의 말뭉치는 문화체육관광부 산하 국립국어원에서 제공하는 말뭉치로
-    총 13 개의 말뭉치로 이뤄져 있습니다.
-    해당 말뭉치를 이용하기 위해서는 국립국어원 홈페이지에 가셔서 "회원가입 > 말뭉치 신청 > 승인"의
-    과정을 거치셔야 합니다.
-    https://corpus.korean.go.kr/#none
-    모두의 말뭉치는 승인 후 다운로드 가능 기간 및 횟수 (3회) 에 제한이 있습니다.
-    로그인 기능 및 Korpora 패키지에서의 다운로드 기능을 제공하려 하였지만,
-    국립국어원에서 위의 이유로 이에 대한 기능은 제공이 불가함을 확인하였습니다.
-    Korpora==0.2.0 에서는 "개별 말뭉치 신청 > 승인"이 완료되었다고 가정,
-    로컬에 다운로드 된 말뭉치를 손쉽게 로딩하는 기능만 제공할 예정입니다
-    (Korpora 개발진 lovit@github, ratsgo@github)"""
-
-license = """    모두의 말뭉치의 모든 저작권은 `문화체육관광부 국립국어원
-    (National Institute of Korean Language)` 에 귀속됩니다.
-    정확한 라이센스는 확인 중 입니다."""
+from .korpus_modu_news import description, license, fetch_modu
 
 
 class ModuMorphemeKorpus(Korpus):
@@ -147,10 +132,3 @@ def load_modu_morpheme(paths):
         documents_iterator = tqdm(documents, desc=desc, total=len(documents))
         examples += [example for doc in documents_iterator for example in document_to_examples(doc)]
     return examples
-
-
-def fetch_modu():
-    raise NotImplementedError(
-        "국립국어원에서 API 기능을 제공해 줄 수 없음을 확인하였습니다."
-        "\n이에 따라 모두의 말뭉치는 fetch 기능을 제공하지 않습니다"
-    )
