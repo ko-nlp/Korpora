871630946c3290839d9ed4632d996a27ab1aa55b
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-03 20:18:18 +0000

    Adding numeric_owner as keyword arguement

--- Korpora/utils.py
+++ Korpora/utils.py
@@ -173,7 +173,7 @@ def web_download_untar(url, tar_path, corpus_name='', force_download=False):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, data_root)
